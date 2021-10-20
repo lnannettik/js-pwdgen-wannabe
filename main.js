4f884418b43706a10a89518dfffe0f8ebf0c2701
lnannettik <l.nannetti.k@gmail.com>
2021-10-20 17:09:54 +0200

    inizio

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+const nomeUtente = prompt('Inserisci il tuo nome');
+console.log
+const cognomeUtente = prompt('Inserisci il tuo cognome');
+
+const coloreUtente = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('risultato').innerHTML = Ó
